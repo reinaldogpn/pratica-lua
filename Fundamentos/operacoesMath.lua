49abb51be117735c4702e6db7143c5c350d5d70b
Reinaldo G. P. Neto <reinaldo.neto@ufv.br>
2023-04-30 05:48:13 -0300

    Add files via upload

---
+++ Fundamentos/operacoesMath.lua
@@ -0,0 +1,23 @@
+a = 10
+b = 2
+
+result = ((a / b) + 10) - 2
+
+print(result)
+
+result = a ^ b
+
+print(result)
+
+-- Operações --
+-- +
+-- -
+-- *
+-- /
+-- ^ (potência)
+
+-- Algumas funções da biblioteca math --
+-- math.abs() (absoluto)
+-- math.ceil() (arredondar para cima)
+-- math.floor() (arredondar para baixo)
+-- math.fmod() (resto da divisão)
